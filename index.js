a29aac3fc3adbf851cc190195440209262fd2c3c
Adarsh Raj <ahazadarsh0014@gmail.com>
2022-06-15 23:20:16 +0530

    jwt tocken enabled

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import cors from "cors"
 import AuthRoute from "./Routes/AuthRoute.js"
 import UserRoute from "./Routes/UserRoute.js"
 import PostRoute from "./Routes/PostRoute.js"
 const app=express();
+
+
 app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+ 
 
+app.use(cors())
 dotenv.config()
 mongoose.connect(process.env.MONGO_DB,
 {useNewUrlParser:true,useUnifiedTopology:true}
